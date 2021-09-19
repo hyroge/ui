cb89da96cd5b8530c8c0ab6236de73a36ba3ca6a
kailun <54568547+KAILUENG@users.noreply.github.com>
2021-09-19 22:38:02 +0800

    α 0.0.0.4.3

--- cui.js
+++ cui.js
@@ -33,3 +33,27 @@ function cuisnackbarshow() {
   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
+
+window.onload = () => {
+  const menu = document.querySelector('.menu')
+  const menuHeight = menu.offsetHeight - parseInt(getComputedStyle(menu)['paddingTop']) - parseInt(getComputedStyle(menu)['paddingBottom'])
+  menu.style.height = '0'
+
+  openMenu = e => {
+      e.preventDefault()
+
+      menu.style.left = `${e.clientX}px`
+      menu.style.top = `${e.clientY + 5}px`
+      menu.style.height = `${menuHeight}px`
+      menu.classList.add('is-active')
+
+      return false
+  }
+
+  colseMenu = () => {
+      menu.style.height = '0'
+      menu.classList.remove('is-active')
+  }
+
+  window.onclick = () => colseMenu()
+}
