97b55e3ea181b1901b539fb8e6a8ff6decbe1afc
kailun <54568547+KAILUENG@users.noreply.github.com>
2021-11-07 16:34:11 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 # connect-UI 「互通设计官方CSS组件库」
-
-## 简介
 众多组件和特色，都在互通设计内，我们期待您的加入，我们知道我们做不到由一个团队完成一个庞大的设计标准，因此，我们邀请您共建互通设计，
 一同设计互通设计的各个组件和布局，完善互通设计的CSS组件库，一同前进吧。
-
-## 要加入我们，或者看看我们的进度？
-https://to-do.microsoft.com/sharing?InvitationToken=N0RDwY0FV4_lHjtgIF9ekAUMGoyHxiPwKn6fab-CnL1nv_DBu02z2MPl-3U7KFViQ 
 
 我们欢迎各位一同构建全新的互通设计和connect UI，互通设计与connect UI均以CC 1.0 协议进行许可。
 * 目前，connect UI仍在早期版本，我们不推荐您使用。
--- cui.js
+++ cui.js
@@ -26,11 +26,72 @@ function cuidroplist() {
 }
 
 /* 实现snackbar */
-
 function cuisnackbarshow() {
-  var x = document.getElementById("cuisnackbar")
+  var x = document.getElementById("cuisnackbar");
   x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
+
+
+window.onload = () => {
+  const menu = document.querySelector('.menu')
+  const menuHeight = menu.offsetHeight - parseInt(getComputedStyle(menu)['paddingTop']) - parseInt(getComputedStyle(menu)['paddingBottom'])
+  menu.style.height = '0'
+
+  openMenu = e => {
+      e.preventDefault()
+
+      menu.style.left = `${e.clientX}px`
+      menu.style.top = `${e.clientY + 5}px`
+      menu.style.height = `${menuHeight}px`
+      menu.classList.add('is-active')
+
+      return false
+  }
+
+  colseMenu = () => {
+      menu.style.height = '0'
+      menu.classList.remove('is-active')
+  }
+
+  window.onclick = () => colseMenu()
+}
+/* 实现snackbar */
+function cuisnackbarshowyellow() {
+  var x = document.getElementById("cuisnackbaryellow");
+  x.className = "show";
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+}
+
+
+window.onload = () => {
+  const menu = document.querySelector('.menu')
+  const menuHeight = menu.offsetHeight - parseInt(getComputedStyle(menu)['paddingTop']) - parseInt(getComputedStyle(menu)['paddingBottom'])
+  menu.style.height = '0'
+
+  openMenu = e => {
+      e.preventDefault()
+
+      menu.style.left = `${e.clientX}px`
+      menu.style.top = `${e.clientY + 5}px`
+      menu.style.height = `${menuHeight}px`
+      menu.classList.add('is-active')
+
+      return false
+  }
+
+  colseMenu = () => {
+      menu.style.height = '0'
+      menu.classList.remove('is-active')
+  }
+
+  window.onclick = () => colseMenu()
+}
+/* 实现snackbar */
+function cuisnackbarshowred() {
+  var x = document.getElementById("cuisnackbarred");
+  x.className = "show";
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
 
 
