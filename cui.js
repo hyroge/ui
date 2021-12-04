88932fa5ef1376ad22c6aea62de156b7195a5a9d
kailun <54568547+KAILUENG@users.noreply.github.com>
2021-12-04 22:30:29 +0800

    update α 0.0.1.3
    1.下拉菜单演示更新
    2.按钮样式更新
    3.input样式更新
    4.新增加cui-textarea，支持多方输入框
    5.新增加cui-big-button（background命名会在下个版本更改）
    6.tab栏重置
    7.cui-pop-up-prompt重置
    8.新增加加载样式的button
    9.重置loading动画
    10.暂时跟侧边栏说再见
    11.新增加cui-search-go（按钮）
    12.新增加cui-helper（多个flex工具）
    13.info card重置
    14.新加图标

--- cui.js
+++ cui.js
@@ -1,25 +1,4 @@
 
-/* 实现手机端tab展开 */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropmenu')) {
-
-    var dropdowns = document.getElementsByClassName("dropmenu-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-
 /* 实现展开菜单 */
 function cuidroplist() {
   document.getElementById("cuidroplist").classList.toggle("listshow");
